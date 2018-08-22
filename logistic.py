fix(sigmoid): you missed the negative sign before np.exp, as part of the sigmoid formula

logistic.py
@@ -3,19 +3,19 @@ import numpy as np
 from train import iteration, return_with_target
 
 
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
+
 class Classifier(object):
     def __init__(self, input_size):
         self.input_size = input_size
         self.W = np.random.random(input_size)
         self.b = np.array([0.])
 
-    def sigmoid(self, input_data):
-        return 1.0 / 1.0 + np.exp(input_data @ self.W + self.b)
-
-    def forward(self, input_data):  # sigmoid
-        # print(type(X))
-        output = np.array(Classifier.sigmoid(self, input_data))
-        return output
+    def forward(self, x):
+        z = x @ self.W + self.b
+        return sigmoid(z)
 
     def backward(self, input_data, output, target):  # backward = back propagation
         # 로지스틱 회귀의 최적화는 backward를 계산하는 것이며 이의 값은 광배법을 위한 값이 된다.